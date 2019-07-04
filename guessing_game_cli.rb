8a1ebf6f48d7803892595ac3cd0f0d51e98c8f78
github username <github email address>
2019-07-04 13:14:56 -0400

    Implementation with if statement passes,

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,24 +1,39 @@
 # Code your solution here!
+# def run_guessing_game
+#   input = ""
+#   num = rand(1...6) + 1
+#   puts "Guess a number between 1 and 6"
+#
+#   while input
+#     # num = rand(1...6) + 1 #works here as well
+#     # puts "Guess a number between 1 and 6"
+#     input = gets.chomp
+#     case input
+#     when num.to_s
+#       puts "You guessed the correct number!"
+#       break
+#     when "exit"
+#       puts "Goodbye!"
+#       break
+#     else
+#       puts "Sorry! The computer guessed #{num}."
+#       break
+#     end
+#   end
+#
+# end
+
 def run_guessing_game
   input = ""
   num = rand(1...6) + 1
-  # puts "Guess a number between 1 and 6"
-  # input = gets.chomp
+  puts "Guess a number between 1 and 6"
+  input = gets.chomp
 
-  while input
-    puts "Guess a number between 1 and 6"
-    input = gets.chomp
-    case input
-    when num.to_s
-      puts "You guessed the correct number!"
-      break
-    when "exit"
-      puts "Goodbye!"
-      break
-    else
-      puts "Sorry! The computer guessed #{num}."
-      break
-    end
+  if input == num.to_s
+    puts "You guessed the correct number!"
+  elsif input == "exit"
+    puts "Goodbye!"
+  else
+    puts "Sorry! The computer guessed #{num}."
   end
-
 end
