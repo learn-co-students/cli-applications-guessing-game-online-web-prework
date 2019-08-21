b44eb19c94cb1734cfd66adf74ca908bdd405e42
github username <github email address>
2019-08-21 03:18:45 -0400

    Done.

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,7 +2,7 @@
 def run_guessing_game
   random_number = 1 + rand(0..6); # rand(1..6) is equivalent doesn't work for some reason
   p("Guess your number between 1 thru 6...");
-  user_input = gets.chomp;
+  user_input = gets.chomp.strip;
   case(user_input.downcase); # The.downcase methnd, which affects strings and is called to catch when user inputs "exit" that's not all in lowercase, doesn't break game when numbers or symbols are inputted
   when(random_number.to_s); # for some reason, the lab excepts a strings
     p("You guessed the correct number!");
