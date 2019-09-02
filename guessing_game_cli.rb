7fc1e476b1ea34806e21a3fe5a4b47b37d6dd858
= <=>
2019-09-01 21:54:00 -0400

    Done.

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,1 +1,12 @@
 # Code your solution here!
+def run_guessing_game
+  number = rand(6) + 1
+  guess = gets.chomp
+  if guess == number.to_s
+    puts "You guessed the correct number!"
+  elsif guess == "exit"
+    puts "Goodbye!"
+  else
+    puts "Sorry! The computer guessed #{number}."
+  end
+end
