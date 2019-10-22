ffc82e4062571a9fea6c979d54717c04fd0a3f45
Romario Gustave <romagustave10@gmail.com>
2019-10-22 04:24:36 +0000

    Done.

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,1 +1,16 @@
 # Code your solution here!
+def run_guessing_game(start)
+end 
+def run_guessing_game
+  generator = rand (1..6)
+  print " Enter a number between 1 to 6 "
+  guess = gets.to_i 
+  if guess == generator 
+    puts "You guessed the correct number!"
+  elsif guess != generator 
+  puts "Sorry! The computer guessed  #{generator}"
+   elsif guess = 'exit'
+   puts "Goodbye!"
+ else 
+   puts "enter a valid number"
+ end
