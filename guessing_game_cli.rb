6b243bdb50a8229ed6dc253717c8637a1d2b2dac
Dan Foley <dan@dan-foley.com>
2019-12-20 05:39:26 +0000

    Done.

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -5,7 +5,6 @@ def run_guessing_game
   secret_num = rand(1..6).to_s
   puts "Pick a number between 1 and 6"
   user_input = gets.chomp
-  binding.pry
   if user_input == "exit"
     puts "Goodbye!"
   elsif user_input != secret_num
